b6ddbfca8ec2d9b21eecc8b3a34df94faf1cf3ee
Manuel Pizarro <manuelpizarro.informatica@gmail.com>
2020-05-25 00:38:12 -0400

    Biblias e interlineal hebreo

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 import os
 from django.shortcuts import render, HttpResponse
-from .models import Book
+from .models import Book, Bible
 from django.http import JsonResponse
 
 import sqlite3
@@ -8,8 +8,16 @@ import sqlite3
 pwd = os.path.dirname(__file__)
 
 def index (request):
-    books = Book.objects.all()
-    return render(request, 'home/index.html', {'books': books, 'n' : range(1,30)})
+    books           = Book.objects.all()
+    bibles          = Bible.objects.all()
+    bibles_active   = Bible.objects.filter(activo=1)
+    colors = ['#145cc9', '#1ac914', '#dc3545']
+    #def replace_values_in_string(text, args_dict):
+    #for key in args_dict.keys():
+    #    text = text.replace(key, str(args_dict[key]))
+    #return text
+
+    return render(request, 'home/index.html', {'books': books, 'n' : range(1,30), 'bibles':bibles, 'bibles_active':bibles_active})
 
 def sql (traduccion, libro, capitulo, verso):
     
@@ -27,14 +35,19 @@ def ajax (request):
     capitulo    = request.POST['capitulo']
     verso       = request.POST['verso']
     
-    torah       = sql ("torah.bblx", libro, capitulo, verso)
-    oso         = sql ("Biblia del Oso.bbli", libro, capitulo, verso)
-    jerusalem   = sql ("Biblia Corona de Jerusaln.bbli", libro, capitulo, verso)
+    hebreo      = sql ("HOT-ALEPPO.bblx", libro, capitulo, verso)
 
-    data = {
-        'success': 'true',
-        'torah':torah,
-        'oso'  :oso,
-        'jerusalem':jerusalem,
-    }
+    args_dict   = { r'\u':'&#', '?':';', r'{\f2':'' }
+    
+    for key in args_dict.keys():
+        hebreo = hebreo.replace(key, str(args_dict[key]))
+
+    bibles_active   = Bible.objects.filter(activo=1)
+    bibles = {}
+    
+    data = {'success': 'true', 'hebreo': hebreo}
+
+    for bible in bibles_active:
+        data["bible"+str(bible.id)] = sql (bible.file, libro, capitulo, verso)
+       
     return JsonResponse(data)
